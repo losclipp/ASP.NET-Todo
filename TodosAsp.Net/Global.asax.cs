3ed292a3cde3328442a9094e74de3bf2d5a86a7f
Sergey Dubovich <sergey.dubovich@aisnovations.com>
2017-11-14 12:20:00 +0300

    + Todo.cs, TodoContext.cs, TodoController.cs, TodoDbInitializer.cs - ValuesContoller.cs * Global.asax.cs

--- TodosAsp.Net/Global.asax.cs
+++ TodosAsp.Net/Global.asax.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using TodosAsp.Net.Models;
 
 namespace TodosAsp.Net
 {
@@ -13,6 +15,8 @@ namespace TodosAsp.Net
 	{
 		protected void Application_Start()
 		{
+			Database.SetInitializer(new TodoDbInitializer());
+
 			AreaRegistration.RegisterAllAreas();
 			GlobalConfiguration.Configure(WebApiConfig.Register);
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
---
+++ TodosAsp.Net/Models/TodoDbInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TodosAsp.Net.Models
+{
+	public class TodoDbInitializer : DropCreateDatabaseAlways<TodoContext> 
+	{
+		protected override void Seed(TodoContext context)
+		{
+			context.Todoes.Add(new Todo { Id = 1, Title = "OLOLO", Project = "Project_1", Completed = false });
+			context.Todoes.Add(new Todo { Id = 2, Title = "XOXOXO", Project = "Project_5", Completed = false });
+			context.Todoes.Add(new Todo { Id = 3, Title = "JUST DO IT", Project = "Project_4", Completed = false });
+			context.Todoes.Add(new Todo { Id = 4, Title = "198543", Project = "Project_4", Completed = false });
+			context.Todoes.Add(new Todo { Id = 5, Title = "BLA-BLA-BLA", Project = "Project_6", Completed = false });
+			context.Todoes.Add(new Todo { Id = 6, Title = "DO-DO-DO", Project = "Project_8", Completed = false });
+			context.Todoes.Add(new Todo { Id = 7, Title = "some title", Project = "Project_11", Completed = false });
+			context.Todoes.Add(new Todo { Id = 8, Title = "^-^", Project = "Project_2", Completed = false });
+			context.Todoes.Add(new Todo { Id = 9, Title = "Real title", Project = "Project_8", Completed = false });
+			context.Todoes.Add(new Todo { Id = 10, Title = "6)", Project = "Project_6", Completed = false });
+		}
+	}
+}
